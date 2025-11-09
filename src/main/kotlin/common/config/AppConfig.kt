3ba176f8cabaad6a9df2107ed0c339b47eea91f1
dalcon10028 <dalcon10280@gmail.com>
2025-11-09 20:39:27 +0900

    Enable transaction management in AppConfig for improved transaction handling

--- src/main/kotlin/common/config/AppConfig.kt
+++ src/main/kotlin/common/config/AppConfig.kt
@@ -2,7 +2,9 @@ package com.example.common.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 open class AppConfig
